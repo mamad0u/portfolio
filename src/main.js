876a02384241db8597aca45f50dbf2f223640e15
Mamadou <mamad33sylla@gmail.com>
2022-08-01 23:28:52 +0200

    debug aos

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const app = createApp(App)
-createApp({
-    mounted() {
-        AOS.init()
-      }
-  })
 
+app.AOS = new AOS.init({ disable: "phone" });
 
-app.mount('#app',AOS.init())
+app.use(AOS).mount('#app')
